fdca0db71e8866c749cc3be427b4488fe79fb66c
goodminkyo <minkyoleewsu@gmail.com>
2019-06-15 17:20:02 +0900

    발화에서 넘겨주는 가수명 키워드를 토대로 API요청 보내기 완성

--- hphk.vlive/code/vliveResponse.js
+++ hphk.vlive/code/vliveResponse.js
@@ -1,46 +1,26 @@
-module.exports.function = function vliveResponse () {
+module.exports.function = function vliveResponse (artistName) {
   
-  /*
-  const http = require('http');
-  const console = require('console');
-  let result = {};
-  let base_url = null;
-  let response = null;
+  const http = require('http')
+  const console = require('console')
+  let base_url = null
+  let response = null
+  let result = []
   
-  base_url = "http://django-env.ibkiuksqzh.us-west-2.elasticbeanstalk.com/api/v1/schedules/v-live/3/?format=json";
-  response = http.getUrl(base_url, {format:"json", cacheTime:1000, returnHeaders:true});
-  
-  console.log(response)
-
+  base_url = "http://django-env.ibkiuksqzh.us-west-2.elasticbeanstalk.com/api/v1/schedules/v-live/channel/" + artistName + "/?format=json"
+  // base_url = "http://django-env.ibkiuksqzh.us-west-2.elasticbeanstalk.com/api/v1/schedules/v-live/?format=json"
+  response = http.getUrl(base_url, {format:"json", cacheTime:1000, returnHeaders:true})
   const ArtistInfo = response.parsed
-  */
   
-  // 더미데이터 만들어서 테스트하기
-  let result = [
-    {
-      "channel": "BTS",
-      "title": "급하게 방송 켰어요 뿌잉!",
-      "startAt": "17:00"
-    },
-    {
-      "channel": "BTS",
-      "title": "얼굴 보실래요?",
-      "startAt": "19:00"
-    },
-    {
-      "channel": "BTS",
-      "title": "야간 치킨먹방 갑니다 ^^",
-      "startAt": "23:00"
-    }
-  ]
-  
-  /*
-  result.title = ArtistInfo.title;
-  result.group = ArtistInfo.group;
-  result.site_url = ArtistInfo.site_url;
-  result.poster_url = ArtistInfo.poster_url;
-  result.start_at = ArtistInfo.start_at;
-  */
-  
+  // ArtistName ()== 가수명) 넣어주기
+  ArtistInfo.forEach( each => {
+    let tempResult = {}
+    tempResult.startAt = each.start_at
+    tempResult.channel = each.channel
+    tempResult.title   = each.title
+    tempResult.imgUrl  = each.img_url
+    tempResult.artistName = artistName
+    result.push(tempResult)
+  })
+
   return result
 }
